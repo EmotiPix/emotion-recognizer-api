ebf19b5b1494c85ea81844d09ce0461a214dbca3
Anna Szczepanska <anna.szczepanska.krk@gmail.com>
2017-12-13 07:43:02 +0100

    add emotion recognition logic

---
+++ web/emotion_handler.py
@@ -0,0 +1,44 @@
+import json
+import urlparse
+import os
+from image_converter import convert_image
+from fishface_classifier import predict_emotion
+
+
+TMP_IMAGE_STORAGE = './tmp_img_storage'
+IMAGE_FILE = TMP_IMAGE_STORAGE + '/img.jpg'
+
+def recognize_emotion(json_data):
+    data = json.loads(json_data)
+    image = data['image']
+    up = urlparse.urlparse(image)
+    head, data = up.path.split(',', 1)
+    plain_data = data.decode("base64")
+
+    if not os.path.exists(TMP_IMAGE_STORAGE):
+        os.makedirs(TMP_IMAGE_STORAGE)
+
+    with open(IMAGE_FILE, 'wb') as f:
+        f.write(plain_data)
+
+    cropped_face, face_detected = convert_image(TMP_IMAGE_STORAGE, IMAGE_FILE)
+    if(face_detected):
+        emotion = predict_emotion(cropped_face)
+    else:
+        emotion = "no_emotion"
+
+    # remove temporarily saved images from TMP_IMAGE_STORAGE
+    directory_cleanup(TMP_IMAGE_STORAGE)
+
+    return emotion
+
+
+
+def directory_cleanup(directory_path):
+    for file in os.listdir(directory_path):
+        file_path = os.path.join(directory_path, file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+        except Exception as e:
+            print(e)
--- web/server.py
+++ web/server.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from flask import request
+from flask_cors import CORS
+from emotion_handler import recognize_emotion
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
-    return 'Tests again'
+    return 'Test'
+
+@app.route("/emotion", methods=["POST"])
+def get_emotion():
+    image_data = request.get_data()
+    return recognize_emotion(image_data)
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
